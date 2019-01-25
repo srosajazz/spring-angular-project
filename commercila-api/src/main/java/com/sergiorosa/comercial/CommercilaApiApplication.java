5401d6b4a4f1ed348fe3b3be04a97f3e6612ac0e
Sergio Rosa <scarlosjazz@gmail.com>
2019-01-25 08:30:21 -0500

    first commit

---
+++ commercila-api/src/main/java/com/sergiorosa/comercial/CommercilaApiApplication.java
@@ -0,0 +1,14 @@
+package com.sergiorosa.comercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercilaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercilaApiApplication.class, args);
+	}
+
+}
+
